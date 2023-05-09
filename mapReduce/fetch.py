236e8a0ecf375b7d81009f608187599ac629d5ae
BBBT888 <yueningteoh@gmail.com>
2023-05-09 16:23:13 +1000

    map reduce

--- mapReduce/fetch.py
+++ mapReduce/fetch.py
@@ -1,12 +1,16 @@
 import requests
 import pandas as pd
 
-db_link = "http://admin:password@172.26.132.147:5984/twitter/_design/view_s1_design/_view/my_filter"
-
-data = requests.get(db_link).json()["rows"]
-
+url = "http://admin:password@172.26.132.147:5984/twitter/_design/view_s1_design/_view/climate_filter?group=true&stale=update_after"
+response = requests.get(url)
+data = response.json()["rows"]
 df = pd.DataFrame(data)
-df = pd.concat([df, pd.json_normalize(df["key"])], axis=1)
-df = df.drop("key", axis=1)
 print(df)
-df.to_csv('df_1.csv')
+df["bbox"] = df["key"].apply(lambda x: x[0])
+df["location"] = df["key"].apply(lambda x: x[1])
+df["avg_sentiment"] = df["value"].apply(lambda x: x[0])
+df["count"] = df["value"].apply(lambda x: x[1])
+df["avg_sentiment"] = df["avg_sentiment"] / df["count"]
+df = df.drop(["key", "value"], axis=1)
+print(df)
+df.to_csv("df_climate.csv")
--- mapReduce/view_s1.js
+++ mapReduce/view_s1.js
@@ -1,12 +1,50 @@
 const fs = require('fs')
 
-const mapReduce = function (tweet) {
+const mapFx = function (doc) {
+  keywords_1 = [
+    "globalwarming",
+    "global warming",
+    "climate",
+    "earthday",
+    "earth day",
+    "pollution",
+    "greenhousegases",
+    "green house gases",
+    "carbondioxide",
+    "carbon dioxide",
+    "co2",
+    "fossilfuels",
+    "fossil fuels",
+    "renewableenergy",
+    "renewable energy",
+    "parisagreement",
+    "paris agreement",
+    "sealevelrise",
+    "sea level rise",
+    "extremeweather",
+    "extreme weather",
+    "sustainability",
+    "cleanenergy",
+    "clean energy",
+    "carbonfootprint",
+    "carbon footprint",
+    "energyefficiency",
+    "energy efficiency",
+    "carboncaptureandstorage",
+    "carbon capture and storage",
+    "carbon capture",
+    "biodiversity",
+    "deforestation",
+    "ipcc",
+    "conferenceofparties",
+    "conference of parties",
+    "emissions",
+    "net-zeroemissions",
+    "carbonpricing",
+    "carbon pricing",
+  ]
 
-  const keywords_1 = [
-    "brexit",
-    "marriage"]
-
-  doc = tweet.doc
+  aus = ['australia', 'australian capital territory', 'new south wales', 'northern territory', 'queensland', 'south australia', 'tasmania', 'victoria', 'western australia']
 
   function correctFormat (input) {
     if (Array.isArray(input)) {
@@ -22,53 +60,40 @@ const mapReduce = function (tweet) {
     return input.toLowerCase()
   }
 
-  if (doc._id) {
-    for (const keyword of keywords_1) {
+  tags = correctFormat(doc.tags)
+  tokens = correctFormat(doc.tokens).replace("|", " ")
+  domain_name = correctFormat(doc.domain_name)
+  description = correctFormat(doc.description)
+  matching_rule_tag = correctFormat(doc.matching_rule_tag)
+  hashtags = correctFormat(doc.hashtags)
+  place_name = correctFormat(doc.place_name).split(", ")[1]
 
-      tags = correctFormat(doc.tags)
-      tokens = correctFormat(doc.tokens)
-      domain_name = correctFormat(doc.domain_name)
-      description = correctFormat(doc.description)
-      text = correctFormat(doc.text)
-      matching_rule_tag = correctFormat(doc.matching_rule_tag)
-      hashtags = correctFormat(doc.hashtags)
-
+  if (aus.includes(place_name)) {
+    for (keyword of keywords_1) {
       if (tags.includes(keyword) ||
         tokens.includes(keyword) ||
         domain_name.includes(keyword) ||
         description.includes(keyword) ||
-        text.includes(keyword) ||
         matching_rule_tag.includes(keyword) ||
         hashtags.includes(keyword)) {
 
-        emit({
-          location: doc.place_name,
-          sentiment: doc.sentiment,
-          tokens: doc.tokens,
-          tags: doc.tags,
-          hashtags: doc.hashtags,
-          tokens: doc.tokens,
-          domain_name: doc.domain_name,
-          description: doc.description,
-          text: doc.text,
-          matching_rule_tags: doc.matching_rule_tag,
-          date: created_at
-        })
+        emit([doc.bbox, doc.place_name], [doc.sentiment, 1])
+
         break
       }
+
     }
   }
-}
 
-const mapReduce_string = mapReduce.toString()
+}
 
 const views = {
   "views": {
-    "my_filter": {
-      "map": mapReduce_string
+    "climate_filter": {
+      "map": mapFx.toString(),
+      "reduce": "_sum"
     }
   }
 }
 
-console.log(views)
 fs.writeFileSync('view_s1.json', JSON.stringify(views))
